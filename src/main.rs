c6e30feee21a9245383628f9468a1b48281110cd
Jonathan Gil <jhongilp@gmail.com>
2023-01-19 19:57:43 -0500

    read arguments from command line and write record into a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().skip(1);
+    let key = args.next().unwrap();
+    let value = args.next().unwrap();
+    println!("The key is {key} and value is {value}");
+
+    let contents = format!("{key}\t{value}\n");
+    std::fs::write("kv.db", contents).unwrap();
 }
