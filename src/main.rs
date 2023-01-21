7b5803f9a36f9abbf196bfdbfdd2638512f47faf
Jonathan Gil <jhongilp@gmail.com>
2023-01-21 15:58:43 -0500

    add database struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+
 fn main() {
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap();
@@ -6,4 +9,15 @@ fn main() {
 
     let contents = format!("{key}\t{value}\n");
     std::fs::write("kv.db", contents).unwrap();
+}
+
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Database {
+        Database { map: HashMap::new(),  }
+    }
 }
