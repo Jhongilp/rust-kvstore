29bb73e118d46a1afdc895d8f2762c5fa7b42f8f
Jonathan Gil <jhongilp@gmail.com>
2023-01-31 19:25:25 -0500

    add new function and insert method to database struct

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
 use std::collections::HashMap;
 
-
 fn main() {
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap();
     let value = args.next().unwrap();
     println!("The key is {key} and value is {value}");
 
-    let contents = format!("{key}\t{value}\n");
-    std::fs::write("kv.db", contents).unwrap();
-}
+    // let contents = format!("{key}\t{value}\n");
+    // std::fs::write("kv.db", contents).unwrap();
 
+    let mut database = Database::new().expect("Database::new() crashed");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value.clone());
+}
 
 struct Database {
     map: HashMap<String, String>,
 }
 
 impl Database {
-    fn new() -> Database {
-        Database { map: HashMap::new(),  }
+    fn new() -> Result<Database, std::io::Error> {
+        let mut map = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?; // almost same pattern as match above
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key!");
+            let value = chunks.next().expect("No value!");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+
+        Ok(Database { map })
+    }
+
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
     }
 }
